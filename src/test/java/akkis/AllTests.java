a93131ba85588daffd391ab0576607b22048260b
Niko <madjoki@gmail.com>
2016-12-19 16:53:16 +0200

    Lisää testipohja

---
+++ src/test/java/akkis/AllTests.java
@@ -0,0 +1,12 @@
+package akkis;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+@RunWith(Suite.class)
+@SuiteClasses({
+	TyhjaTesti.class,
+})
+public class AllTests {
+}
---
+++ src/test/java/akkis/TyhjaTesti.java
@@ -0,0 +1,14 @@
+package akkis;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class TyhjaTesti {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
