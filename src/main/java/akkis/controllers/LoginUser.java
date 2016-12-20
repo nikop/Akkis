644a29e18f8110b72dee703639880c8b36c2e271
Niko <madjoki@gmail.com>
2016-12-20 12:57:37 +0200

    Login

--- src/main/java/akkis/controllers/LoginUser.java
+++ src/main/java/akkis/controllers/LoginUser.java
@@ -9,5 +9,20 @@ import javax.persistence.Entity;
 public class LoginUser {
 
 	private String user;
+	private String password;
+	
+	public String getUser() {
+		return user;
+	}
+	public void setUser(String user) {
+		this.user = user;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	
 	
 }
