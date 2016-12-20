5faf66f38f2fa4150f29392221128b7dee21cd61
Niko <madjoki@gmail.com>
2016-12-20 12:48:08 +0200

    add login

---
+++ src/main/java/akkis/controllers/LoginUser.java
@@ -0,0 +1,13 @@
+package akkis.controllers;
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.RequestScoped;
+import javax.persistence.Entity;
+
+@ManagedBean
+@RequestScoped
+public class LoginUser {
+
+	private String user;
+	
+}
