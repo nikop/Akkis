a826d3a969e09fe0b87be158654ab85a1276eb0d
Niko <madjoki@gmail.com>
2017-03-02 13:21:08 +0200

    Use mock for InvoiceRow

--- src/test/java/akkis/InvoiceTest.java
+++ src/test/java/akkis/InvoiceTest.java
@@ -1,8 +1,10 @@
 package akkis;
 
 import static org.junit.Assert.*;
+import static org.mockito.Mockito.*;
 
 import org.junit.Test;
+
 
 public class InvoiceTest {
 
@@ -10,12 +12,20 @@ public class InvoiceTest {
 	public void test() {
 		Invoice inv = new Invoice();
 		
-		inv.addRow(new InvoiceRow("Item 1", 10));
-		inv.addRow(new InvoiceRow("Item 2", 1, 10));
-		inv.addRow(new InvoiceRow("Item 3", 2, 10));
+		InvoiceRow row1 = mock(InvoiceRow.class);
+		InvoiceRow row2 = mock(InvoiceRow.class);
+		InvoiceRow row3 = mock(InvoiceRow.class);
 		
-		assertEquals(40.0, inv.calculateSum(), 0.0);
-		assertEquals(40.0, inv.getSum(), 0.0);
+		when(row1.getRowTotal()).thenReturn(10.0);
+		when(row2.getRowTotal()).thenReturn(20.0);
+		when(row3.getRowTotal()).thenReturn(30.0);
+		
+		inv.addRow(row1);
+		inv.addRow(row2);
+		inv.addRow(row3);
+		
+		assertEquals(60.0, inv.calculateSum(), 0.0);
+		assertEquals(60.0, inv.getSum(), 0.0);
 	}
 
 }
